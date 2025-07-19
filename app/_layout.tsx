9c9dd5d3a95a836943f1c3029a2ff9700732ee31
Mohammad Talim <talimmohammad116@gmail.com>
2025-07-19 17:08:53 +0530

    Set up bottom tabs navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,37 @@
-import { Stack } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Shopping List" }} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="list" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="lightbulb" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="clock" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,9 @@
-import { Link } from "expo-router";
 import { StyleSheet, View } from "react-native";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Link href="/counter" style={{ fontSize: 24 }}>
-        Counter
-      </Link>
-      <Link href="/idea">Idea</Link>
       <ShoppingListItem name="Coffee" />
       <ShoppingListItem name="Tea" isCompleted />
       <ShoppingListItem name="Sugar" isCompleted />
